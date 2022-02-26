4e1a44cd4aefb55b7f6d90564a5c76fff570a48e
Damian Salvardor <akabuezendudiha@gmail.com>
2022-02-26 20:16:30 +0100

    Second commit

---
+++ src/Create.js
@@ -0,0 +1,60 @@
+import {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Shubam');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+
+        setIsPending(true);
+
+        fetch('', {
+            method: 'POST',
+            headers: {"Content-type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            setIsPending(false);
+            history.push('/');
+        })
+    }
+
+    return (
+        <div className="create">
+            <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog Title</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Body:</label>
+                <textarea 
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                />
+                <label>Blog Author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Shubham">Shubham</option>
+                    <option value="Satyam">Satyam</option>
+                    <option value="Anmol">Anmol</option>
+                </select>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog</button>}
+            </form>
+        </div>
+    );   
+}
+
+export default Create;
